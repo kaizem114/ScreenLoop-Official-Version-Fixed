df0a8ef0c63bda415d0d19496cb458bd12313136
kaizem114 <zemmourizakaria97@gmail.com>
2026-08-03 23:58:31 -0400

    Add Player Components
    Just added useful components like movespeed value and some basic vectors

--- Assets/New Folder/Scripts/Player/movement/PlayerMovement.cs
+++ Assets/New Folder/Scripts/Player/movement/PlayerMovement.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-{
+{//Player Infos
+    public float Move_speed;
+    public Rigidbody Player_rb;
+    public AudioSource Player_ears;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,9 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 camForward = transform.forward;
+        Vector3 camRight = transform.right;
+
+
     }
 }
