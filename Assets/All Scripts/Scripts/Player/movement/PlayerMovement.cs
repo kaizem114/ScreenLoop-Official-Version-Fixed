c4ee9983533894dd1d01f0485120737500cca30b
thetrueadam3-art <thetrueadam3@gmail.com>
2026-08-18 18:57:15 -0400

    Adding basic movement and camera setup
    The movement is kinda slow, but the player wont need sprinting in a tight environment

--- Assets/All Scripts/Scripts/Player/movement/PlayerMovement.cs
+++ Assets/All Scripts/Scripts/Player/movement/PlayerMovement.cs
@@ -7,18 +7,69 @@ public class PlayerMovement : MonoBehaviour
     public float Move_speed;
     public Rigidbody Player_rb;
     public AudioSource Player_ears;
+    public float sensY;
+    public float sensX;
+    public Transform orientation;
+    float xRotation;
+    float yRotation;
+    public GameObject cam;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Camera Initial Setup
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+        yRotation += mouseX;
+
+        xRotation -= mouseY;
+
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        //Rotate Camera
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        transform.rotation = Quaternion.Euler(0, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+
+
+
         Vector3 camForward = transform.forward;
         Vector3 camRight = transform.right;
+        
+        //basic movement
+        //Move Forward
+        if (Input.GetKey(KeyCode.W))
+        {
+
+            transform.position += Move_speed * Time.deltaTime * camForward.normalized;
+
+        }
+        //Move Backward
+        if (Input.GetKey(KeyCode.S))
+        {
+
+            transform.position += Move_speed * Time.deltaTime * -camForward.normalized;
+
+        }
+        //Move Left
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Move_speed * Time.deltaTime * -camRight.normalized;
 
 
+        }
+        //Move Right
+        if (Input.GetKey(KeyCode.D))
+        {
+
+            transform.position += Move_speed * Time.deltaTime * camRight.normalized;
+
+        }
     }
 }
