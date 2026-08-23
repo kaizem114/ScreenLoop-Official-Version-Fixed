aae7bf998446e3d25c3e604e440ddc8fef428121
kaizem114 <zemmourizakaria97@gmail.com>
2026-08-23 12:33:31 -0400

    add new functions

---
+++ Assets/All Scripts/Scripts/OS script/os_system.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class os_system : MonoBehaviour
+{
+    public GameObject lobby;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        //Start With the main menu, all of the time.
+        
+
+    }
+
+    public void Gotointernet()
+    {
+
+        lobby.gameObject.SetActive(false);
+    }
+    //Give access to internet
+
+
+    //Go to the Minigame
+
+
+
+    //Turn on or off the computer
+  
+    void Update()
+    {
+        
+
+
+    }
+}
